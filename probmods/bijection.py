9f3b00a42e3699c290233e18440823c12e6cac75
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2021-07-04 23:23:16 +0100

    Add first versions of interface and bijections

---
+++ probmods/bijection.py
@@ -0,0 +1,330 @@
+import inspect
+import re
+
+import lab as B
+from matrix import AbstractMatrix, TiledBlocks
+from plum import Union, Dispatcher, Tuple
+
+__all__ = [
+    "Bijection",
+    "Composition",
+    "Normaliser",
+    "Log",
+    "Squishing",
+    "parse",
+]
+
+_dispatch = Dispatcher()
+
+_Numeric = Union[B.Numeric, AbstractMatrix]
+
+
+class Bijection:
+    """A bijection."""
+
+    def transform(self, x, *args):
+        """Transform `x`. You can also run `transform` by simply calling the object.
+
+        Args:
+            x (object): Objects to transform.
+            i (int, optional): Index of `x` to transform.
+
+        Returns:
+            object: Transformed version of `x`.
+        """
+        return transform(self, x, *args)
+
+    def __call__(self, *args):
+        return self.transform(*args)
+
+    def untransform(self, y, *args):
+        """Untransform `y`.
+
+        Args:
+            y (object): Object to untransform.
+            i (int, optional): Index of `y` to untransform.
+
+        Returns:
+            object: Original version of `y` before the transform.
+        """
+        return untransform(self, y, *args)
+
+    def logdet(self, x, *args):
+        """Compute the log-determinant of the Jacobian of the transform.
+
+        Args:
+            x (object): Object to transform.
+            i (int, optional): Index of `x` to transform.
+
+        Returns:
+            scalar: Log-determinant of the Jocobian at `x`.
+        """
+        return logdet(self, x, *args)
+
+
+class Composition(Bijection):
+    """Composition of bijections.
+
+    Args:
+        *bijections (:class:`.Bijection`)
+    """
+
+    def __init__(self, *bijections):
+        self.bijections = bijections
+
+
+@_dispatch
+def transform(b: Composition, x, *args):
+    for bi in reversed(b.bijections):
+        x = transform(bi, x, *args)
+    return x
+
+
+@_dispatch
+def untransform(b: Composition, y, *args):
+    for bi in b.bijections:
+        y = untransform(bi, y, *args)
+    return y
+
+
+@_dispatch
+def logdet(b: Composition, y, *args):
+    logdet_sum = 0
+    for bi in b.bijections[:-1]:
+        logdet_sum = logdet_sum + logdet(bi, y, *args)
+        y = untransform(bi, y, *args)
+    return logdet_sum + logdet(b.bijections[-1], y, *args)
+
+
+def _extend_i(f):
+    signature = inspect.signature(f)
+    T1, T2 = [signature.parameters[p].annotation for p in list(signature.parameters)]
+
+    def extension(b: T1, x: T2, i: B.Int):
+        return f(b, x)
+
+    extension.__name__ = f.__name__
+    extension.__qualname__ = f.__qualname__  # Important for dispatch!
+    _dispatch(extension)
+    return f
+
+
+class Identity(Bijection):
+    """Identity bijection."""
+
+
+@_dispatch
+@_extend_i
+def transform(b: Identity, x):
+    return x
+
+
+@_dispatch
+@_extend_i
+def untransform(b: Identity, y):
+    return y
+
+
+@_dispatch
+@_extend_i
+def logdet(b: Identity, y):
+    return B.zero(y)
+
+
+class Normaliser(Bijection):
+    """Create a data normaliser."""
+
+    def __init__(self):
+        self._fit = False
+        self._mean = None
+        self._scale = None
+
+    def _get_mean_scale(self, x, i=None, fit=False):
+        if self._fit:
+            if i is None:
+                return self._mean, self._scale
+            else:
+                return self._mean[0, i], self._scale[0, i]
+        else:
+            if fit:
+                self._mean = B.nanmean(x, axis=0, squeeze=False)
+                self._scale = B.nanstd(x, axis=0, squeeze=False)
+                self._fit = True
+                return self._get_mean_scale(x, i)
+            else:
+                if i is None:
+                    dtype = B.dtype(x)
+                    n = B.shape(x, 0)
+                    return B.zeros(dtype, n, 1), B.ones(dtype, n, 1)
+                else:
+                    return B.zero(x), B.one(x)
+
+
+@_dispatch
+def transform(b: Normaliser, x: _Numeric):
+    mean, scale = b._get_mean_scale(x, fit=True)
+    return (x - mean) / scale
+
+
+@_dispatch
+def transform(b: Normaliser, x: _Numeric, i: B.Int):
+    mean, scale = b._get_mean_scale(x, i)
+    return (x - mean) / scale
+
+
+@_dispatch
+def transform(b: Normaliser, x: TiledBlocks):
+    # `x` is a variance.
+    mean, scale = b._get_mean_scale(x)
+    return TiledBlocks(
+        *[
+            (block / scale / B.transpose(scale), rep)
+            for block, rep in zip(x.blocks, x.reps)
+        ],
+        axis=x.axis,
+    )
+
+
+@_dispatch
+def untransform(b: Normaliser, y: _Numeric):
+    mean, scale = b._get_mean_scale(y)
+    return y * scale + mean
+
+
+@_dispatch
+def untransform(b: Normaliser, y: _Numeric, i: B.Int):
+    mean, scale = b._get_mean_scale(y, i)
+    return y * scale + mean
+
+
+@_dispatch
+def untransform(b: Normaliser, y: Tuple[_Numeric, _Numeric]):
+    mean, scale = b._get_mean_scale(y)
+    dist_mean, dist_var = y
+    if B.rank(dist_var) == 2:
+        return (
+            dist_mean * scale + mean,
+            dist_var * scale ** 2,
+        )
+    elif B.rank(dist_var) == 3:
+        return (
+            dist_mean * scale + mean,
+            dist_var * scale[:, :, None] * scale[:, None, :],
+        )
+    else:
+        raise ValueError(f"Invalid rank {B.rank(dist_var)} of the variance.")
+
+
+@_dispatch
+def logdet(b: Normaliser, y: _Numeric):
+    _, scale = b._get_mean_scale(y)
+    return -B.shape(y, 0) * B.sum(B.log(scale))
+
+
+@_dispatch
+def logdet(b: Normaliser, y: _Numeric, i: B.Int):
+    _, scale = b._get_mean_scale(y, i)
+    return -B.shape(y, 0) * B.log(scale)
+
+
+class Log(Bijection):
+    """Log-transform for positive data."""
+
+
+@_dispatch
+@_extend_i
+def transform(b: Log, x: _Numeric):
+    return B.log(x)
+
+
+@_dispatch
+@_extend_i
+def untransform(b: Log, y: _Numeric):
+    return B.exp(y)
+
+
+@_dispatch
+def untransform(b: Log, y: Tuple[_Numeric, _Numeric]):
+    mean, var = y
+    if B.rank(var) == 2:
+        pass
+    elif B.rank(var) == 3:
+        raise NotImplementedError(f"Log-transform not implemented for joint variances.")
+    else:
+        raise ValueError(f"Invalid rank {B.rank(var)} of the variance.")
+    # These are the mean and variance of the log-normal distribution.
+    return (
+        B.exp(mean + 0.5 * var),
+        (B.exp(var) - 1) * B.exp(2 * mean + var),
+    )
+
+
+@_dispatch
+@_extend_i
+def logdet(b: Log, y: _Numeric):
+    return -B.sum(y)
+
+
+class Squishing(Bijection):
+    """Squishing transform for real-valued, heavy-tailed data."""
+
+
+@_dispatch
+@_extend_i
+def transform(b: Squishing, x: _Numeric):
+    return B.sign(x) * B.log(1 + B.abs(x))
+
+
+@_dispatch
+@_extend_i
+def untransform(b: Squishing, y: _Numeric):
+    return B.sign(y) * (B.exp(B.abs(y)) - 1)
+
+
+@_dispatch
+@_extend_i
+def logdet(b: Squishing, y: _Numeric):
+    return -B.sum(B.abs(y))
+
+
+@_dispatch
+def parse(spec: str):
+    """Build a bijection from a string specification.
+
+    The specifications "normaliser" or "normalise" build a normalising transformation.
+    The specifications "log" or "positive" build a log-transform. The specification
+    "squishing" builds a squishing transformation. These transformations can be composed
+    with a comma or plus, e.g. "normaliser,log" or "normaliser+log". Specifications are
+    case insensitive.
+
+    Args:
+        spec (str): Specification.
+
+    Returns:
+        :class:`.Bijection`: Bijection corresponding to specification.
+    """
+    if not spec:
+        return Identity()
+
+    parts = [part.strip().lower() for part in re.split(r"[,+]", spec)]
+    bijections = []
+    for part in parts:
+        if part in {"normaliser", "normalise"}:
+            bijections.append(Normaliser())
+        elif part in {"log", "positive"}:
+            bijections.append(Log())
+        elif part == "squishing":
+            bijections.append(Squishing())
+        else:
+            raise ValueError(f'Unknown bijection "{part}".')
+    return Composition(*bijections)
+
+
+@_dispatch
+def parse(bijection: Bijection):
+    return bijection
+
+
+@_dispatch
+def parse(bijection: type(None)):
+    return Identity()
---
+++ probmods/model.py
@@ -0,0 +1,373 @@
+import abc
+import contextlib
+from functools import wraps
+from types import FunctionType
+from typing import Union
+
+from lab import B
+from plum import Dispatcher
+from varz import Vars
+
+__all__ = [
+    "ProbabilisticModel",
+    "MultiOutputModel",
+]
+
+_dispatch = Dispatcher()
+
+
+@_dispatch
+def _same_framework(dtype, a: B.Numeric):
+    a_dtype = B.dtype(a)
+    # Only check for TensorFlow, PyTorch, and JAX.
+    return any(
+        [
+            isinstance(dtype, DType) and isinstance(a_dtype, DType)
+            for DType in [B.TFDType, B.TorchDType, B.JAXDType]
+        ]
+    )
+
+
+@_dispatch
+def _same_framework(dtype, a):
+    # Do nothing if `a` is not numeric.
+    return False
+
+
+@_dispatch
+def _convert_input(dtype, rank, a: B.Numeric):
+    return B.to_active_device(B.uprank(B.cast(dtype, a), rank=rank))
+
+
+@_dispatch
+def _convert_input(dtype, rank, a: Union[bool, int, float]):
+    # Do not convert certain builtins.
+    return a
+
+
+@_dispatch
+def _convert_input(dtype, rank, a):
+    # Do nothing if `a` is not numeric.
+    return a
+
+
+@_dispatch
+def _on_gpu_if_possible(dtype: B.NPDType):
+    return contextlib.suppress()
+
+
+@_dispatch
+def _on_gpu_if_possible(dtype: B.TorchDType):
+    import torch
+
+    if torch.cuda.is_available():
+        return B.on_device("cuda")
+    else:
+        return contextlib.suppress()
+
+
+@_dispatch
+def _on_gpu_if_possible(dtype: B.TFDType):
+    import tensorflow as tf
+
+    if len(tf.config.list_physical_devices("gpu")) >= 1:
+        return B.on_device("gpu")
+    else:
+        return contextlib.suppress()
+
+
+@_dispatch
+def _on_gpu_if_possible(dtype: B.JAXDType):
+    import jax
+
+    gpus = [d for d in jax.devices() if "gpu" in str(d).lower()]
+    if len(gpus) >= 1:
+        return B.on_device(gpus[0])
+    else:
+        return contextlib.suppress()
+
+
+@_dispatch
+def _convert_output_to_np(x: B.Numeric):
+    return B.to_numpy(x)
+
+
+@_dispatch
+def _convert_output_to_np(xs: list):
+    return [_convert_output_to_np(x) for x in xs]
+
+
+@_dispatch
+def _convert_output_to_np(xs: tuple):
+    return tuple(_convert_output_to_np(x) for x in xs)
+
+
+@_dispatch
+def _convert_output_to_np(d: dict):
+    return {k: _convert_output_to_np(v) for k, v in d.items()}
+
+
+@_dispatch
+def _convert_output_to_np(x):
+    # Default to not converting.
+    return x
+
+
+def create_argument_converter(rank=2, autogpu=True):
+    """Create a decorator which automatically converts argument to the right framework
+    and the right data type, converts the tensors to at least a given rank, and
+    automatically converts back the output to NumPy if none of the arguments were of the
+    same right framework. It also automatically runs the method on the GPU if one is
+    available.
+
+    Args:
+        rank (int, optional): Minimum rank of arguments. Default to `2`.
+        autogpu (bool, optional): Automatically run the method on the GPU if one is
+            available.
+
+    Returns:
+        function: Decorator.
+    """
+
+    def decorator(f):
+        @wraps(f)
+        def f_wrapped(self, *args, **kw_args):
+            # Convert the result to NumPy if none of the arguments or keyword arguments
+            # are in the same framewor as `dtype`.
+            res_to_np = not (
+                any(_same_framework(self.dtype, arg) for arg in args)
+                or any(_same_framework(self.dtype, v) for v in kw_args.values())
+            )
+
+            def convert_and_run():
+                # Perform conversion and run wrapped function.
+                converted_args = tuple(
+                    _convert_input(self.dtype, rank, arg) for arg in args
+                )
+                convert_kw_args = {
+                    k: _convert_input(self.dtype, rank, v) for k, v in kw_args.items()
+                }
+                return f(self, *converted_args, **convert_kw_args)
+
+            if autogpu:
+                # Run on the GPU, if possible.
+                with _on_gpu_if_possible(self.dtype):
+                    res = convert_and_run()
+            else:
+                res = convert_and_run()
+
+            if res_to_np:
+                return _convert_output_to_np(res)
+            else:
+                return res
+
+        return f_wrapped
+
+    return decorator
+
+
+convert_arguments = create_argument_converter(rank=2, autogpu=True)
+"""Decorator: Common instance of the argument converter."""
+
+
+class ProbabilisticModelMeta(abc.ABCMeta):
+    def __new__(cls, name, bases, dict_):
+        instance = abc.ABCMeta.__new__(cls, name, bases, dict_)
+        skip_methods = instance.__abstractmethods__
+        for k, v in dict_.items():
+            is_private_method = k.startswith("_")
+            if (
+                isinstance(v, FunctionType)
+                and k not in skip_methods
+                and not is_private_method
+            ):
+                setattr(instance, k, convert_arguments(v))
+        return instance
+
+
+class ProbabilisticModel(metaclass=ProbabilisticModelMeta):
+    """A probabilistic model.
+
+    Args:
+        dtype (dtype): Data type.
+    """
+
+    def __init__(self, dtype):
+        self.dtype = dtype
+        self._vs_source = _Source(Vars(dtype))
+
+    @abc.abstractmethod
+    def logpdf(self, x, y):
+        """Compute the logpdf of observations.
+
+        Args:
+            x (tensor): Inputs of observations.
+            y (tensor): Outputs of observations.
+
+        Returns:
+            scalar: The logpdf.
+        """
+
+    @abc.abstractmethod
+    def condition(self, x, y):
+        """Condition the model on observations.
+
+        Args:
+            x (tensor): Inputs of observations.
+            y (tensor): Outputs of observations.
+
+        Returns:
+            :class:`.ProbabilisticModel`: A posterior version of the model.
+        """
+
+    @abc.abstractmethod
+    def fit(self, x, y, **kw_args):
+        """Fit the model.
+
+        Takes in further keyword arguments which will be passed to an optimiser.
+
+        Args:
+            x (tensor): Inputs of observations.
+            y (tensor): Outputs of observations.
+        """
+
+    @abc.abstractmethod
+    def sample(self, x):
+        """Sample from the model.
+
+        Args:
+            x (tensor): Inputs to sample at.
+
+        Returns:
+            tensor: Samples from the prior.
+        """
+
+    @abc.abstractmethod
+    def predict(self, x):
+        """Make predictions.
+
+        Args:
+            x (tensor): Inputs to predict at.
+
+        Returns:
+            object: Predictions.
+        """
+
+    @property
+    def vs(self):
+        """:class:`varz.Vars`: Variable container."""
+        return self._vs_source.vs
+
+    @vs.setter
+    @_dispatch
+    def vs(self, source: Vars):
+        self._vs_source.vs = source
+
+    @property
+    def params(self):
+        """:class:`varz.spec.Struct`: Parameter struct."""
+        return self._vs_source.params
+
+    @_dispatch
+    def use_vs(self, vs: Vars):
+        """Use different variables.
+
+        Args:
+            vs (:class:`varz.Vars`): Variable container.
+
+        Returns:
+            context: Context in which `self` uses different variables.
+        """
+        return _SetVS(vs, self)
+
+    @_dispatch
+    def bind(self, model: "ProbabilisticModel"):
+        self._vs_source = _Reference(model)
+        return self
+
+    @_dispatch
+    def bind(self, model: "ProbabilisticModel", attr: str):
+        self._vs_source = _Reference(model, attr)
+        return self
+
+    @_dispatch
+    def __setattr__(self, key, value):
+        object.__setattr__(self, key, value)
+
+    @_dispatch
+    def __setattr__(self, key, model: "ProbabilisticModel"):
+        model.bind(self, key)
+        object.__setattr__(self, key, model)
+
+
+class _AbstractSource:
+    pass
+
+
+class _Source(_AbstractSource):
+    def __init__(self, vs):
+        self._vs = vs
+
+    @property
+    def vs(self):
+        return self._vs
+
+    @vs.setter
+    def vs(self, vs):
+        self._vs = vs
+
+    @property
+    def params(self):
+        return self.vs.struct
+
+
+class _Reference(_AbstractSource):
+    def __init__(self, source, attr=None):
+        self._source = source
+        self._attr = attr
+
+    @property
+    def vs(self):
+        return self._source.vs
+
+    @vs.setter
+    def vs(self, vs):
+        self._source.vs = vs
+
+    @property
+    def params(self):
+        params = self._source.params
+        if self._attr:
+            return getattr(params, self._attr)
+        else:
+            return params
+
+
+class _SetVS:
+    def __init__(self, vs, model):
+        self.vs = vs
+        self.old_vs = None
+        self.model = model
+
+    def __enter__(self):
+        self.old_vs = self.model.vs
+        self.model.vs = self.vs
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.model.vs = self.old_vs
+
+
+class MultiOutputModel(ProbabilisticModel):
+    """A probabilistic model with multiple outputs."""
+
+    @property
+    def num_outputs(self):
+        """int: Number of outputs."""
+        if hasattr(self, "_num_outputs") and self._num_outputs is not None:
+            return self._num_outputs
+        else:
+            raise RuntimeError("Number of outputs is requested, but not (yet) defined.")
+
+    @num_outputs.setter
+    def num_outputs(self, num):
+        self._num_outputs = num
